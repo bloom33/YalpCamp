adda503b7c2584812e6c96cd762bdb55f4ebbe54
Chibuba Bloom Osuala <bloom33@Chibubas-MBP.attlocal.net>
2023-05-24 11:12:09 -0500

    Used router.route to group related routes in campgrounds and users route files.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,27 +12,32 @@ const { isLoggedIn, validateCampground, isAuthorized } = require("../middleware"
 const campground = require("../models/campground");
 
 //*** ROUTES ***/
-//Camprounds Index Page Route
-router.get("/", wrapAsync(campgrounds.index));
+//Use 'router.route' to group routes and further consolidate and clean up code.
+
+//*** Group Routes for '/' ***//
+router
+  .route("/")
+  //Camprounds Index Page Route
+  .get(wrapAsync(campgrounds.index))
+  //New campground Submit Route (and Redirect)
+  .post(isLoggedIn, validateCampground, wrapAsync(campgrounds.newCamp));
 
 //New Campground Form Route
 //remember: don't need an async callback for creating a new item since there is nothing to wait for beforehand
 //NOTE!: The create route needs to come BEFORE the show route, otherwise the server will search - and not find - an item with the id of 'new'
 router.get("/new", isLoggedIn, campgrounds.newForm);
 
-//New campground Submit Route (and Redirect)
-router.post("/", isLoggedIn, validateCampground, wrapAsync(campgrounds.newCamp));
-
-//Campground Details Page Route
-router.get("/:id", wrapAsync(campgrounds.showCamp));
+//*** Group Routes for '/:id' ***//
+router
+  .route("/:id")
+  //Campground Details Page Route
+  .get(wrapAsync(campgrounds.showCamp))
+  //Camprgound Edit Submission Route and Redirect
+  .put(isLoggedIn, isAuthorized, validateCampground, wrapAsync(campgrounds.updateCamp))
+  //Delete Route
+  .delete(isLoggedIn, isAuthorized, wrapAsync(campgrounds.deleteCamp));
 
 //Edit Campground Route
 router.get("/:id/edit", isLoggedIn, isAuthorized, wrapAsync(campgrounds.editCamp));
-
-//Camprgound Edit Submission Route and Redirect
-router.put("/:id", isLoggedIn, isAuthorized, validateCampground, wrapAsync(campgrounds.updateCamp));
-
-//Delete Route
-router.delete("/:id", isLoggedIn, isAuthorized, wrapAsync(campgrounds.deleteCamp));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -11,26 +11,29 @@ const passport = require("passport");
 //require controller file with user routes
 const users = require("../controllers/usercontrol");
 
-//Route to render/display User Registration form
-router.get("/register", users.reForm);
+//*** Group Routes for '/register' ***//
+router
+  .route("/register")
+  //Route to render/display User Registration form
+  .get(users.reForm)
+  //Route to which user reg form is posted
+  .post(wrapAsync(users.regUser));
 
-//Route to which user reg form is posted
-router.post("/register", wrapAsync(users.regUser));
-
-//Route to render/display User Login form
-router.get("/login", users.loginForm);
-
-//Route  to which user login form is posted
-router.post(
-  "/login",
-  //Passport automaticaaly comes with a method called '.autheticate()' to which you can pass what type of authentication (local, gogle, twitter, etc) and validator options
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-    keepSessionInfo: true,
-  }),
-  users.login
-);
+//*** Group Routes for '/register' ***//
+router
+  .route("/login")
+  //Route to render/display User Login form
+  .get(users.loginForm)
+  //Route  to which user login form is posted
+  .post(
+    //Passport automaticaaly comes with a method called '.autheticate()' to which you can pass what type of authentication (local, gogle, twitter, etc) and validator options
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+      keepSessionInfo: true,
+    }),
+    users.login
+  );
 
 //Logout route
 router.get("/logout", users.logout);
