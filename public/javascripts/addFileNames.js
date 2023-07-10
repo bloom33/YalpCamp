c26ff9122c73e8c4f793e05d0dc36fea282f42f2
Chibuba Bloom Osuala <bloom33@Chibubas-MBP.attlocal.net>
2023-07-09 19:44:18 -0500

    Tried another solution; now individual file names display - though no images; left unnecessary files in case I can figure out why other code wasn't working or t at least figure out a way to add image preview as well.

--- public/javascripts/addFileNames.js
+++ public/javascripts/addFileNames.js
@@ -1,32 +1,32 @@
-function addFileNames(e) {
-  const form = document.querySelector("#formFile");
-  form.innerHTML = "";
-  let images = document.getElementById("image"); //selecting 'image' id from ejs new and edit forms
-  let num = images.files.length; //
-  for (i = 0; i < num; i++) {
-    let urls = URL.createObjectURL(e.target.files[i]); //
-    document.getElementById("formFile").innerHTML += `<img src="${urls}">`;
-    //
-  }
-}
-
 // function addFileNames(e) {
-//   // select imgPreview div where imgs will be displayed:
-//   const imgPreview = document.querySelector("#imgPreview");
-//   // clear previous images:
-//   imgPreview.innerHTML = "";
-//   // loop over files and add to imgPreview div:
-//   for (i = 0; i < e.target.files.length; i++) {
-//     const urls = URL.createObjectURL(e.target.files[i]);
-//     const fileName = e.target.files[i].name;
-//     imgPreview.innerHTML += `<figure class="figure mt-2 col-4 col-lg-3">
-// <img class="img-thumbnail" src="${urls}"><figcaption class="figure-caption">${fileName}</figcaption></figure>
-// `;
+//   const form = document.querySelector("#formFile");
+//   form.innerHTML = "";
+//   let images = document.getElementById("image"); //selecting 'image' id from ejs new and edit forms
+//   let num = images.files.length; //
+//   for (i = 0; i < num; i++) {
+//     let urls = URL.createObjectURL(e.target.files[i]); //
+//     document.getElementById("formFile").innerHTML += `<img src="${urls}">`;
+//     //
 //   }
 // }
 
-// Listens for a change on file input and if there is a change, calls preview function
-document.querySelector("#imgUpload").addEventListener("change", (e) => {
-  if (!e.target.files) return; // Do nothing.
-  addFileNames(e);
-});
+// // function addFileNames(e) {
+// //   // select imgPreview div where imgs will be displayed:
+// //   const imgPreview = document.querySelector("#imgPreview");
+// //   // clear previous images:
+// //   imgPreview.innerHTML = "";
+// //   // loop over files and add to imgPreview div:
+// //   for (i = 0; i < e.target.files.length; i++) {
+// //     const urls = URL.createObjectURL(e.target.files[i]);
+// //     const fileName = e.target.files[i].name;
+// //     imgPreview.innerHTML += `<figure class="figure mt-2 col-4 col-lg-3">
+// // <img class="img-thumbnail" src="${urls}"><figcaption class="figure-caption">${fileName}</figcaption></figure>
+// // `;
+// //   }
+// // }
+
+// // Listens for a change on file input and if there is a change, calls preview function
+// document.querySelector("#imgUpload").addEventListener("change", (e) => {
+//   if (!e.target.files) return; // Do nothing.
+//   addFileNames(e);
+// });
